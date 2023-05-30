0b1eb374d0654b1f3ff6ad8e281959b0b0ea335c
LeoNardelli84 <siblings.distribuidora@gmail.com>
2023-05-30 01:16:05 -0300

    correccion menu derecha animaciones

--- js/cositas.js
+++ js/cositas.js
@@ -1,3 +1,5 @@
+var usuario = localStorage.getItem('nombre');
+
 $(".progress").click(function(){
 
     var barra = $(this).children();
@@ -32,4 +34,14 @@ $(".progress").click(function(){
   
    
   
+  });
+
+  $(".menu-derecha").click(function(){
+    var typed = new Typed('#typed-menu-der', {
+      strings: [ usuario+ ' te cuento un poco sobre mi, estudie programacion de forma autodidacta por siemple curiosidad, me encanta dibujar y me encanta crear cosas, la programacion web me brindaba lo mejor de los dos mundos... Asi que aca estoy, intentando aprender para poder dedicarme a lo que me apasiona.'],
+      typeSpeed: 30,
+      showCursor: false,
+      loop: false,
+  
+    },);
   });
---
+++ js/estudios.js
@@ -0,0 +1,11 @@
+var usuario = localStorage.getItem('nombre');
+
+$(".menu-derecha").click(function(){
+    var typed = new Typed('#typed-menu-der', {
+      strings: [ usuario+ ' te cuento un poco sobre mi, estudie programacion de forma autodidacta por siemple curiosidad, me encanta dibujar y me encanta crear cosas, la programacion web me brindaba lo mejor de los dos mundos... Asi que aca estoy, intentando aprender para poder dedicarme a lo que me apasiona.'],
+      typeSpeed: 30,
+      showCursor: false,
+      loop: false,
+  
+    },);
+  });
