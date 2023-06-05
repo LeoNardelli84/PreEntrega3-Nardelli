969e338fd34fe65b64b8f6464573e3247fd29e99
LeoNardelli84 <siblings.distribuidora@gmail.com>
2023-06-04 21:10:33 -0300

    falta pages estudios, index y cositas

--- js/librerias.js
+++ js/librerias.js
@@ -25,7 +25,7 @@ var typed = new Typed('#typed-ejemplo', {
 //eventos click
 $(".menu-derecha").click(function(){
   var typed = new Typed('#typed-menu-der', {
-    strings: [ usuario+ ' te cuento un poco sobre mi, estudie programacion de forma autodidacta por siemple curiosidad, me encanta dibujar y me encanta crear cosas, la programacion web me brindaba lo mejor de los dos mundos... Asi que aca estoy, intentando aprender para poder dedicarme a lo que me apasiona.'],
+    strings: [ usuario+ ' la elección de librerias y frameworks a utilizar es clave, no todos los proyectos necesitan lo mismo. Una buena captación de las necesitades del cliente son escenciales para brindarle una mejor solución.'],
     typeSpeed: 30,
     showCursor: false,
     loop: false,
--- js/roadmap.js
+++ js/roadmap.js
@@ -2,3 +2,12 @@ var usuario = localStorage.getItem('nombre');
 
 $("#usuario").html(usuario).css('text-transform', 'capitalize');
 
+$(".menu-derecha").click(function(){
+    var typed = new Typed('#typed-menu-der', {
+      strings: [ usuario+ ' este fue el orden en el que decidi estudiar estas tecnologias, entre medio aprendi varias herramientas para optimizar la velocidad de carga del sitio, como <a href="https://pagespeed.web.dev">PageSpeed insights de Google.</a>'],
+      typeSpeed: 30,
+      showCursor: false,
+      loop: false,
+  
+    },);
+  });
