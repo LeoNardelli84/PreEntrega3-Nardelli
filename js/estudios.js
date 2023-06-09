00dc4f012e76c504d6a4a8b8dc685d32302a9ca5
LeoNardelli84 <siblings.distribuidora@gmail.com>
2023-06-08 23:32:51 -0300

    terminano estudios.hmtl

--- js/estudios.js
+++ js/estudios.js
@@ -1,5 +1,7 @@
 var usuario = localStorage.getItem('nombre');
 
+$(".usuario").html(usuario);
+
 $(".menu-derecha").click(function(){
     var typed = new Typed('#typed-menu-der', {
       strings: [ usuario+ ' te cuento un poco sobre mi, estudie programacion de forma autodidacta por siemple curiosidad, me encanta dibujar y me encanta crear cosas, la programacion web me brindaba lo mejor de los dos mundos... Asi que aca estoy, intentando aprender para poder dedicarme a lo que me apasiona.'],
